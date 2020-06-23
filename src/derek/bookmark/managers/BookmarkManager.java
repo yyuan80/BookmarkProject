1650420a4183b530b9759ca0201379b696e8d249
Derek Yuan <yyuan80@outlook.com>
2020-06-23 17:26:50 -0400

    created basic constants and entities

---
+++ src/derek/bookmark/managers/BookmarkManager.java
@@ -0,0 +1,56 @@
+//Singleton Pattern
+package derek.bookmark.managers;
+
+import derek.bookmark.entities.Book;
+import derek.bookmark.entities.Movie;
+import derek.bookmark.entities.WebLink;
+
+public class BookmarkManager {
+	private static BookmarkManager instance = new BookmarkManager();
+
+	private BookmarkManager() {
+	}
+
+	public static BookmarkManager getInstance() {
+		return instance;
+	}
+
+	public WebLink createWebLink(Long id, String title, String url, String host) {
+		WebLink weblink = new WebLink();
+		weblink.setId(id);
+		weblink.setTitle(title);
+		weblink.setUrl(url);
+		weblink.setHost(host);
+
+		return weblink;
+	}
+
+	public Book createBook(long id, String title, int publicationYear, String publisher, String[] authors, String genre,
+			double amazonRating) {
+		Book book = new Book();
+		book.setId(id);
+		book.setTitle(title);
+		book.setPublicationYear(publicationYear);
+		book.setPublisher(publisher);
+		book.setAuthors(authors);
+		book.setGenre(genre);
+		book.setAmazonRating(amazonRating);
+		
+		return book;
+	}
+
+	public Movie createMovie(long id, String title, String profileUrl, int releaseYear, String[] cast,
+			String[] directors, String genre, double imdbRating) {
+		Movie movie = new Movie();
+		movie.setId(id);
+		movie.setTitle(title);
+		movie.setProfileUrl(profileUrl);
+		movie.setReleaseYear(releaseYear);
+		movie.setCast(cast);
+		movie.setDirectors(directors);
+		movie.setGenre(genre);
+		movie.setImdbRating(imdbRating);
+
+		return movie;
+	}
+}
---
+++ src/derek/bookmark/managers/UserManager.java
@@ -0,0 +1,29 @@
+//Singleton Pattern
+package derek.bookmark.managers;
+
+import derek.bookmark.entities.User;
+
+public class UserManager {
+	private static UserManager instance = new UserManager();
+
+	private UserManager() {
+	}
+
+	public static UserManager getInstance() {
+		return instance;
+	}
+
+	public User createUser(long id, String email, String password, String firstName, String lastName, int gender,
+			String userType) {
+		User user = new User();
+		user.setId(id);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setGender(gender);
+		user.setUserType(userType);
+		
+		return user;
+	}
+}
